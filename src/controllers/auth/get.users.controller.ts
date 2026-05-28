f12f5e0cc58206979e68e8f9dac74e1c65a6b98e
Anas Ismail <100934197+AnasIsmail8996@users.noreply.github.com>
2026-05-29 01:24:15 +0500

    Deploy these changes and the admin login, client page-refresh, and dashboard data fetching

--- src/controllers/auth/get.users.controller.ts
+++ src/controllers/auth/get.users.controller.ts
@@ -4,7 +4,7 @@ import UserModel from "../../models/user.model";
 export const getAllUsers = async (req: Request, res: Response) => {
     try {
         const users = await UserModel.find().select("-password");
-        return res.status(200).json(users);
+        return res.status(200).json({ success: true, users });
     } catch (error: any) {
         return res.status(500).json({
             success: false,
